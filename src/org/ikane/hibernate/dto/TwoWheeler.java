4c89d2d110bcbfadcc8c2eb55edfbb86ba76590e
ikane <kaneelhadji2610@yahoo.fr>
2013-06-25 16:12:48 +0200

    Hibernate Tutorial 19 - Implementing Inheritance With Table Per Class Strategy

--- src/org/ikane/hibernate/dto/TwoWheeler.java
+++ src/org/ikane/hibernate/dto/TwoWheeler.java
@@ -4,7 +4,6 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
-@DiscriminatorValue("BIKE")
 public class TwoWheeler extends Vehicle {
 	
 	private String steeringHandle;
--- src/org/ikane/hibernate/dto/Vehicle.java
+++ src/org/ikane/hibernate/dto/Vehicle.java
@@ -16,11 +16,10 @@ import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
 @Entity
-@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name="VEHICLE_TYPE", discriminatorType=DiscriminatorType.STRING)
+@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
 public class Vehicle {
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	@Id
+	
+	@Id @GeneratedValue(strategy=GenerationType.TABLE)
 	private int vehicleId;
 	private String vehicleName;
 	
